d6b458b7def7ded8162e2515c279dfe2642bbfb9
Ricardo Katz <rikatz@users.noreply.github.com>
2021-11-28 21:48:34 -0300

    Refactor yaml reader and add stdin reading (#209)

--- pkg/kubepug/input/file/utils.go
+++ pkg/kubepug/input/file/utils.go
@@ -1,6 +1,7 @@
 package fileinput
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"io"
@@ -8,10 +9,10 @@ import (
 	"os"
 	"strings"
 
-	log "github.com/sirupsen/logrus"
-	yaml "gopkg.in/yaml.v3"
-
 	"github.com/rikatz/kubepug/pkg/results"
+	log "github.com/sirupsen/logrus"
+
+	"sigs.k8s.io/yaml"
 )
 
 // FileStruct defines a type that will receive a common format of objects regardless of the input format (yaml, json)
@@ -20,8 +21,8 @@ type FileStruct struct {
 	APIVersion string `yaml:"apiVersion"`
 	Metadata   struct {
 		Name      string `yaml:"name"`
-		Namespace string `yaml:"namespace,omitempty"`
-	}
+		Namespace string `yaml:"namespace"`
+	} `yaml:"metadata"`
 }
 
 // FileItems is a map which key is the Group/Version/Kind from K8s and value are the items found in
@@ -30,10 +31,15 @@ type FileItems map[string][]results.Item
 
 // GetFileItems converts a bunch of input files into a map of Items
 func GetFileItems(location string) (fileItems FileItems) {
-	var filesInfo []os.FileInfo
-
 	fileItems = make(FileItems)
 	// First we get the list of files
+
+	if location == "-" {
+		fileItems.yamlToMap(nil, "-", false)
+		return fileItems
+	}
+
+	var filesInfo []os.FileInfo
 
 	fileLocation, err := os.Stat(location)
 	if os.IsNotExist(err) {
@@ -41,7 +47,7 @@ func GetFileItems(location string) (fileItems FileItems) {
 	}
 
 	if fileLocation.IsDir() {
-		filesInfo, err = ioutil.ReadDir(location)
+		filesInfo, err = ioutil.ReadDir(location) // Too lazy to refactor right now :P
 		if err != nil {
 			log.Fatalf("error to read input location %s. Error: %v", location, err)
 		}
@@ -63,54 +69,66 @@ func (fileItems FileItems) yamlToMap(file os.FileInfo, location string, isDir bo
 		location = fmt.Sprintf("%s/%s", location, file.Name())
 	}
 
-	yamlFile, err := ioutil.ReadFile(location)
-	if err != nil {
-		log.Warningf("Failed to read file %s: %v. Skipping to next file", location, err)
+	var err error
+	var yamlFiles []byte
+	if location == "-" {
+		reader := bufio.NewReader(os.Stdin)
+		yamlFiles, err = io.ReadAll(reader)
+		if err != nil {
+			log.Warningf("Unable to read from STDIN: %s", err)
+			return
+		}
+		location = "STDIN" // Changing here just to be beautified in the list
+	} else {
+		yamlFiles, err = os.ReadFile(location)
+		if err != nil {
+			log.Warningf("Unable to read manifest file, skipping: %s", err)
+			return
+		}
+	}
+
+	yamlObjects := bytes.Split(yamlFiles, []byte("---"))
+
+	for _, yamlObject := range yamlObjects {
+		var obj FileStruct
+		err := yaml.Unmarshal(yamlObject, &obj)
+		if err != nil {
+			log.Warningf("Found invalid yaml: %v. Skipping to next", err)
+			continue
+		}
+		fileItems.addObject(&obj, location)
+	}
+}
+
+func (fileItems FileItems) addObject(obj *FileStruct, location string) {
+	var group, version, objIndex string
+
+	gv := strings.Split(obj.APIVersion, "/")
+	if len(gv) > 1 {
+		group = gv[0]
+		version = gv[1]
+		objIndex = fmt.Sprintf("%s/%s/%s", group, version, obj.Kind)
+	} else {
+		version = gv[0]
+		objIndex = fmt.Sprintf("%s/%s", version, obj.Kind)
+	}
+
+	if version == "" || obj.Kind == "" {
+		log.Infof("YAML file does not contain apiVersion or Kind: %s  Skipping to next", location)
 		return
 	}
 
-	dec := yaml.NewDecoder(bytes.NewReader(yamlFile))
+	item := results.Item{
+		ObjectName: obj.Metadata.Name,
+		Namespace:  obj.Metadata.Namespace,
+		Location:   location,
+		Scope:      "OBJECT",
+	}
 
-	for {
-		var obj FileStruct
-		if err = dec.Decode(&obj); err != nil && err != io.EOF {
-			log.Warningf("Found invalid yaml: %s: %v. Skipping to next", location, err)
-			break
-		}
-		if err == io.EOF {
-			return
-		}
-
-		var group, version, objIndex string
-
-		gv := strings.Split(obj.APIVersion, "/")
-		if len(gv) > 1 {
-			group = gv[0]
-			version = gv[1]
-			objIndex = fmt.Sprintf("%s/%s/%s", group, version, obj.Kind)
-		} else {
-			group = ""
-			version = gv[0]
-			objIndex = fmt.Sprintf("%s/%s", version, obj.Kind)
-		}
-
-		if version == "" || obj.Kind == "" {
-			log.Warningf("YAML file does not contain apiVersion or Kind: %s  Skipping to next", location)
-			break
-		}
-
-		item := results.Item{
-			ObjectName: obj.Metadata.Name,
-			Namespace:  obj.Metadata.Namespace,
-			Location:   location,
-			Scope:      "OBJECT",
-		}
-
-		if items, ok := fileItems[objIndex]; !ok {
-			fileItems[objIndex] = []results.Item{item}
-		} else {
-			items = append(items, item)
-			fileItems[objIndex] = items
-		}
+	if items, ok := fileItems[objIndex]; !ok {
+		fileItems[objIndex] = []results.Item{item}
+	} else {
+		items = append(items, item)
+		fileItems[objIndex] = items
 	}
 }
