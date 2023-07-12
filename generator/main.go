b7c86313b4ed3dc841cafd05863036c3bc4fb00e
Ricardo Katz <ricardo.katz@gmail.com>
2023-07-12 10:18:12 -0300

    Add apideprecation action and page

---
+++ generator/main.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// This generator is based on Kubernetes' prerelease-lifecycle-gen
+// but is being used to generate files to be consumed externally (like Kubepug, but also
+// eventually to generate a better deprecations page)
+
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+
+	deprecationsgenerator "github.com/rikatz/kubepug/generator/deprecations"
+
+	"github.com/spf13/pflag"
+	"k8s.io/gengo/args"
+	"k8s.io/klog/v2"
+)
+
+func main() {
+	klog.InitFlags(nil)
+
+	genericArgs := args.Default().WithoutDefaultFlagParsing()
+
+	genericArgs.AddFlags(pflag.CommandLine)
+	flag.Set("logtostderr", "true")
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+
+	regGenerator := deprecationsgenerator.NewAPIRegistry()
+
+	if err := genericArgs.Execute(
+		deprecationsgenerator.NameSystems(),
+		deprecationsgenerator.DefaultNameSystem(),
+		regGenerator.Packages,
+	); err != nil {
+		klog.Fatalf("Error: %v", err)
+	}
+
+	registries := regGenerator.Registry()
+	data, err := json.Marshal(registries)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(data))
+}
--- go.mod
+++ go.mod
@@ -7,12 +7,15 @@ require (
 	github.com/goccy/go-json v0.10.2
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.7.0
+	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.8.4
 	golang.org/x/mod v0.12.0
 	gopkg.in/yaml.v3 v3.0.1
 	k8s.io/apimachinery v0.27.3
 	k8s.io/cli-runtime v0.27.3
 	k8s.io/client-go v0.27.3
+	k8s.io/gengo v0.0.0-20210813121822-485abfe95c7c
+	k8s.io/klog/v2 v2.90.1
 	sigs.k8s.io/release-utils v0.7.4
 	sigs.k8s.io/yaml v1.3.0
 )
@@ -51,7 +54,6 @@ require (
 	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/xlab/treeprint v1.1.0 // indirect
 	go.starlark.net v0.0.0-20220328144851-d1966c6b9fcd // indirect
 	golang.org/x/net v0.9.0 // indirect
@@ -60,12 +62,12 @@ require (
 	golang.org/x/term v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/time v0.0.0-20220609170525-579cf78fd858 // indirect
+	golang.org/x/tools v0.7.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	k8s.io/api v0.27.3 // indirect
-	k8s.io/klog/v2 v2.90.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230501164219-8b0f38b5fd1f // indirect
 	k8s.io/utils v0.0.0-20230209194617-a36077c30491 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
