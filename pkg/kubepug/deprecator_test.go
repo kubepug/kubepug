adf61f678427412505ea9d480a3b2d4c34b2ad76
Ricardo Katz <ricardo.katz@gmail.com>
2023-08-07 10:00:46 -0300

    Add some more unit tests

--- pkg/kubepug/deprecator_test.go
+++ pkg/kubepug/deprecator_test.go
@@ -1,31 +1,89 @@
 package kubepug
 
 import (
-	"reflect"
+	"fmt"
 	"testing"
 
+	"github.com/rikatz/kubepug/pkg/apis/v1alpha1"
 	"github.com/rikatz/kubepug/pkg/results"
+	"github.com/stretchr/testify/require"
 )
 
+var (
+	deprecatedMock = []results.ResultItem{
+		{
+			Group:   "something.anything",
+			Version: "v1beta1",
+			Kind:    "blah",
+			Replacement: &v1alpha1.GroupVersionKind{
+				Group:   "something.anything",
+				Version: "v1",
+				Kind:    "blah",
+			},
+			K8sVersion: "v1.22",
+			Items: []results.Item{
+				{
+					Scope:      "Object",
+					Namespace:  "default",
+					ObjectName: "bloh",
+				},
+			},
+		},
+	}
+
+	deletedMock = []results.ResultItem{
+		{
+			Group:   "something.anything",
+			Version: "v1alpha1",
+			Kind:    "blah",
+			Replacement: &v1alpha1.GroupVersionKind{
+				Group:   "something.anything",
+				Version: "v1",
+				Kind:    "blah",
+			},
+			K8sVersion: "v1.19",
+			Items: []results.Item{
+				{
+					Scope:      "Object",
+					Namespace:  "default",
+					ObjectName: "bloh",
+				},
+			},
+		},
+	}
+)
+
+type mockStore struct {
+	deprecated  []results.ResultItem
+	deleted     []results.ResultItem
+	shouldError bool
+}
+
+func (m *mockStore) GetDeprecations() (deprecated, deleted []results.ResultItem, err error) {
+	if m.shouldError {
+		return nil, nil, fmt.Errorf("something weird happened")
+	}
+	return m.deprecated, m.deleted, nil
+}
+
 func TestGetDeprecations(t *testing.T) {
-	tests := []struct {
-		name       string
-		d          Deprecator
-		wantResult results.Result
-		wantErr    bool
-	}{
-		// TODO: Implement Mock deprecations and add tests.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotResult, err := GetDeprecations(tt.d)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetDeprecations() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(gotResult, tt.wantResult) {
-				t.Errorf("GetDeprecations() = %v, want %v", gotResult, tt.wantResult)
-			}
-		})
-	}
+	t.Run("should return an error", func(t *testing.T) {
+		store := &mockStore{shouldError: true}
+		result, err := GetDeprecations(store)
+		require.Error(t, err)
+		require.Empty(t, result.DeletedAPIs)
+		require.Empty(t, result.DeprecatedAPIs)
+	})
+
+	t.Run("should return correctly", func(t *testing.T) {
+		store := &mockStore{
+			shouldError: false,
+			deprecated:  deprecatedMock,
+			deleted:     deletedMock,
+		}
+		result, err := GetDeprecations(store)
+		require.NoError(t, err)
+		require.Equal(t, deletedMock, result.DeletedAPIs)
+		require.Equal(t, deprecatedMock, result.DeprecatedAPIs)
+	})
 }
--- pkg/utils/downloader_test.go
+++ pkg/utils/downloader_test.go
@@ -1,1 +1,67 @@
 package utils
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	someRandomContent = "testing download"
+)
+
+func TestDownloadGeneratedJSON(t *testing.T) {
+	ts := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path == "/data.json" {
+				w.Header().Add("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(someRandomContent)) //nolint: errcheck
+			}
+			if r.URL.Path == "/notfound.json" {
+				w.WriteHeader(http.StatusNotFound)
+			}
+		}),
+	)
+	defer ts.Close()
+
+	t.Run("force bad tempdir", func(t *testing.T) {
+		// Force OSEnv to wrong tmpdir
+		err := os.Setenv("TMPDIR", "/xpto123lalala")
+		t.Cleanup(func() {
+			err = os.Setenv("TMPDIR", "")
+			require.NoError(t, err)
+		})
+		require.NoError(t, err)
+		f, err := DownloadGeneratedJSON(ts.URL + "/data.json")
+		require.Error(t, err)
+		require.Empty(t, f)
+	})
+
+	t.Run("download valid data", func(t *testing.T) {
+		f, err := DownloadGeneratedJSON(ts.URL + "/data.json")
+		require.NoError(t, err)
+		require.Contains(t, f, "kubepug")
+		require.Contains(t, f, "data.json")
+	})
+
+	t.Run("error on invalid data", func(t *testing.T) {
+		f, err := DownloadGeneratedJSON(ts.URL + "/notfound.json")
+		require.Error(t, err)
+		require.Empty(t, f)
+	})
+
+	t.Run("error on invalid url", func(t *testing.T) {
+		f, err := DownloadGeneratedJSON("http://127.0.0.1:xpto1")
+		require.Error(t, err)
+		require.Empty(t, f)
+	})
+
+	t.Run("try to create file on a forbidden place", func(t *testing.T) {
+		err := downloadFile("/tmp123/xpto", ts.URL+"/data.json")
+		require.Error(t, err)
+	})
+}
